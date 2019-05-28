a3f16317d2a4a9340b70748545e4e72b5e2aa945
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-05-28 11:46:18 -0700

    getting user data back

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // implement your API here
+
+
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.post("/api/users" , (req, res) => {
+    const param = req.body;
+    console.log(param)
+    db.insert(param).then((result) => {
+       res.json({ result });
+    })
+})
+
+server.get("/api/users" , (req, res) => {
+    db.find().then((data) => {
+        res.json({ data });
+    }).catch(() => {
+        res.statusCode(500).send({ error: "The users information could not be retrieved." })
+    })
+})
+
+server.listen(5000, () =>
+  console.log('Server running on http://localhost:5000')
+);
